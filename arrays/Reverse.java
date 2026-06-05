661747e9f3c3015cb4301c43dbba40deb7cb91fc
Geetha-005 <chillageetha323@gmail.com>
2026-06-05 10:28:05 +0530

    array is reversed and optimised code

--- arrays/Reverse.java
+++ arrays/Reverse.java
@@ -4,5 +4,7 @@ class Reverse{
     public static void main(String[] args) {
         
         System.out.println("reversing an array ");
+        System.out.println("reversing an array dode by  front end developer geetha ");
+        
     }
 }
