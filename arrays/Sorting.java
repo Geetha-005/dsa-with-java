d0fe5f3a4e6d7aca42233a314363fd183866d138
Geetha-005 <chillageetha323@gmail.com>
2026-06-05 10:34:31 +0530

    backend logic adding

--- arrays/Sorting.java
+++ arrays/Sorting.java
@@ -5,5 +5,7 @@ class Sorting{
     public static void main(String[] args) {
         
         System.out.println("reversing an array ");
+        System.out.println("arrays is sorted using the bubble sort ");
+        System.out.println("developed by backend geetha developer ");
     }
 }
